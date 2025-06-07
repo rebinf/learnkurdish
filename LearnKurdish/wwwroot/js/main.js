681067647cea5664d2b51863fa0716775ab457b2
Rebin <rebinf@outlook.com>
2025-06-07 23:39:22 +0100

    update

--- LearnKurdish/wwwroot/js/main.js
+++ LearnKurdish/wwwroot/js/main.js
@@ -27,57 +27,61 @@ function closeNav()
     document.getElementById("menu-btn").style.left = "0";
 }
 
-var tds = document.querySelectorAll("td, th");
-var groups = [];
-
-for (var i = 0; i < tds.length; i++)
+function StripeTables()
 {
-    if (tds[i].getAttribute('rowspan') != null)
-    {
-        var rspan = tds[i];
-        groups.push({
-            parent: rspan.parentNode,
-            height: rspan.getAttribute('rowspan')
-        });
-    }
-}
+    var tds = document.querySelectorAll("td, th");
+    var groups = [];
 
-var count = 0;
-var rows = document.querySelectorAll('tr');
-var dark = true;
-
-for (var i = 0; i < rows.length; i++)
-{
-    var row = rows[i];
-    var index = groupIndex(row);
-    if (index != null && dark)
+    for (var i = 0; i < tds.length; i++)
     {
-        var group = groups[index];
-        var height = parseInt(group.height);
-        for (var j = i; j < i + height; j++)
+        if (tds[i].getAttribute('rowspan') != null)
         {
-            rows[j].classList.add('dark');
+            var rspan = tds[i];
+            groups.push({
+                parent: rspan.parentNode,
+                height: rspan.getAttribute('rowspan')
+            });
         }
-        i += height - 1;
+    }
+
+    var count = 0;
+    var rows = document.querySelectorAll('tr');
+    var dark = true;
+
+    for (var i = 0; i < rows.length; i++)
+    {
+        var row = rows[i];
+        var index = groupIndex(row);
+        if (index != null && dark)
+        {
+            var group = groups[index];
+            var height = parseInt(group.height);
+            for (var j = i; j < i + height; j++)
+            {
+                rows[j].classList.add('dark');
+            }
+            i += height - 1;
+            dark = !dark;
+            continue;
+        }
+        if (dark)
+        {
+            rows[i].classList.add('dark');
+        }
         dark = !dark;
-        continue;
     }
-    if (dark)
+
+    function groupIndex(element)
     {
-        rows[i].classList.add('dark');
+        for (var i = 0; i < groups.length; i++)
+        {
+            var group = groups[i].parent;
+            if (group == element)
+            {
+                return i;
+            }
+        }
+        return null;
     }
-    dark = !dark;
 }
 
-function groupIndex(element)
-{
-    for (var i = 0; i < groups.length; i++)
-    {
-        var group = groups[i].parent;
-        if (group == element)
-        {
-            return i;
-        }
-    }
-    return null;
-}
